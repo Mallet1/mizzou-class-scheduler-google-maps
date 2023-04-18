edd1a067426bb668eb87a0d07b320cd959cf974b
Mallet1 <88162723+Mallet1@users.noreply.github.com>
2023-04-18 15:55:36 -0500

    init commit

---
+++ components/distance.tsx
@@ -0,0 +1,31 @@
+const commutesPerYear = 260 * 2;
+const litresPerKM = 10 / 100;
+const gasLitreCost = 1.5;
+const litreCostKM = litresPerKM * gasLitreCost;
+const secondsPerDay = 60 * 60 * 24;
+
+type DistanceProps = {
+  leg: google.maps.DirectionsLeg;
+};
+
+export default function Distance({ leg }: DistanceProps) {
+  if (!leg.distance || !leg.duration) return null;
+
+  const days = Math.floor(
+    (commutesPerYear * leg.duration.value) / secondsPerDay
+  );
+
+  const cost = Math.floor(
+    (leg.distance.value / 1000) * litreCostKM * commutesPerYear
+  );
+
+  return (
+    <div>
+      <p>
+        This home is <span className="highlight">{leg.distance.text}</span>
+      </p>{" "}
+      away from youir office. That would take{" "}
+      <span className="highlight">{leg.duration.text}</span> by foot.
+    </div>
+  );
+}
