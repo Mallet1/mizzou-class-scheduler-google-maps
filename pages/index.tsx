cdf9b8d2893f1662030679057afb627e25e01727
mallet1 <smallet079@gmail.com>
2025-03-04 12:24:13 -0600

    init commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Map from "../components/map";
 
 export default function Home() {
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    googleMapsApiKey: "AIzaSyDztZPUFRI-wFIjJ-RCdqM0SyNPheA3pEo", // process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
     libraries: ["places"],
   });
 
